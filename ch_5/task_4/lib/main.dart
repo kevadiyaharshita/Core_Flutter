e837e64a37a5c1f8360878b28ffcc4a97df8e43d
3264harshitakevadiya <harshitakevadiya@gmail.com>
2023-05-24 23:27:15 +0530

    Chapter 5.1Task

---
+++ ch_5/task_4/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Mashal"),
+          centerTitle: true,
+          backgroundColor: Colors.brown,
+        ),
+        body: Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown,
+              border: Border(
+                top:BorderSide(
+                  width: 20,
+                  color: Colors.brown.shade300,
+                ),
+                bottom: BorderSide(
+                  width: 20,
+                  color: Colors.brown.shade300,
+                ),
+                left: BorderSide(
+                  width: 30,
+                  color: Colors.white,
+                ),
+                right: BorderSide(
+                  width: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            alignment: Alignment(0,-2.70),
+            child: Text("🔥",style: TextStyle(fontSize: 35),),
+          ),
+        ),
+      ),
+    ),
+  );
+}
