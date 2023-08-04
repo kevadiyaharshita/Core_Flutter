1141ba6f6925c09cdfe8e109cfd17a0a5a61c6f8
3264harshitakevadiya <harshitakevadiya@gmail.com>
2023-08-04 18:53:38 +0530

    Quote App

--- ch_8/quotes_app/lib/main.dart
+++ ch_8/quotes_app/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quotes_app/Views/screens/Details_page.dart';
 import 'package:quotes_app/utils/MyRoutes.dart';
 import 'package:quotes_app/utils/color_utils.dart';
 
 import 'Views/screens/Home_Pade.dart';
+import 'Views/screens/Splash_screen.dart';
 
-void main()
-{
+void main() {
   runApp(
     const MyApp(),
   );
@@ -17,15 +18,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: theme_1,
-      ),
-
-    routes: {
-      MyRoutes.home: (context) => const HomePage(),
-    }
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: theme_1,
+        ),
+        initialRoute: MyRoutes.Splash_Screen,
+        routes: {
+          MyRoutes.home: (context) => const HomePage(),
+          MyRoutes.detail_page: (context) => const Detail_Page(),
+          MyRoutes.Splash_Screen: (context) => const Splash_Screen(),
+        });
   }
 }
--- ch_8/quotes_app/lib/utils/MyRoutes.dart
+++ ch_8/quotes_app/lib/utils/MyRoutes.dart
@@ -1,4 +1,5 @@
-class MyRoutes
-{
+class MyRoutes {
   static String home = '/';
+  static String detail_page = 'Detail_Page';
+  static String Splash_Screen = 'Splash_screen';
 }
